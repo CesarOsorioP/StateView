27abae1bd710ac151c477739c1cb443a4f514d3e
Santiago Gesama Valencia <sgesama3@gmail.com>
2025-03-07 18:00:45 -0500

    Organize frontend structure and components

--- stateview/src/index.js
+++ stateview/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles/app.css"; 
+import App from "./App"; // Punto principal de tu aplicación
 
 const root = ReactDOM.createRoot(document.getElementById("root")); 
 root.render(<App />);
