689e302ea7273834a81ebfae2b6e6ce84597e33a
CesarOsorioP <osoriocesar024@gmail.com>
2025-05-05 18:35:02 -0500

    import api

--- client/src/components/Series/Series.jsx
+++ client/src/components/Series/Series.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import "./Series.css";
-import "../api/api.js"
+import "./api/api"
 
 const Series = () => {
   const [series, setSeries] = useState([]);
